77e01b2895219e6ee4a7843fd095e87d7b8b8cf4
Gilsabo <gilsala_88@hotmail.com>
2024-08-08 18:34:44 +0200

    Add padding input

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,7 +1,7 @@
 export const SearchInput = ({ searchQuery, setSearchQuery }) => {
   return (
     <input
-      className="border rounded border-green-800 mr-4"
+      className="border rounded border-green-800 mr-4 p-1"
       type="text"
       placeholder="Search for a country..."
       value={searchQuery}
