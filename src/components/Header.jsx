766c69a64fd94683154ffe1924738ed3bb0bedb8
Gilsabo <gilsala_88@hotmail.com>
2024-08-11 07:11:14 +0200

    Add underline animation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="flex justify-center mt-12 pb-6">
-      <h1 className="text-3xl font-bold text-green-800 hover:text-4xl hover:transition duration-700 ease-in-out">
-        <Link to="/">Countries App</Link>
+      <h1 className="text-3xl font-bold text-green-800">
+        <Link
+          to="/"
+          className="relative inline-block text-green-800 before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-0 before:bg-green-800 before:transition-all before:duration-500 before:ease-in-out hover:before:w-full hover:before:left-0"
+        >
+          Countries App
+        </Link>
       </h1>
     </header>
   );
