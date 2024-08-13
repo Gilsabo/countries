a545da8a0742f16e42a98c70d4a0c00775a1ae10
Gilsabo <gilsala_88@hotmail.com>
2024-08-13 06:59:01 +0200

    Add more margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="flex justify-center mt-12 pb-6">
-      <h1 className="text-3xl font-bold text-green-800">
+      <h1 className="text-3xl font-bold text-green-800 pt-6">
         <Link
           to="/"
           className="relative inline-block text-green-800 before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-0 before:bg-green-800 before:transition-all before:duration-500 before:ease-in-out hover:before:w-full hover:before:left-0"
